95085a783aedb8df835719970dfd6893b7041c03
Nathan Roberts <nathanr@tardislabs.com>
2026-01-13 02:30:27 -0800

    Now using EntityJoinLevelEvent to save players from plague
    This triggers both on respawn, and when a player joins the world. In the
    process we are now ignoring players that spawn below the plague height,
    or someplace other than the overworld.

--- src/main/java/com/tardislabs/plaguesky/events/RespawnEvent.java
+++ src/main/java/com/tardislabs/plaguesky/events/RespawnEvent.java
@@ -4,21 +4,24 @@ import com.tardislabs.plaguesky.Config;
 import com.tardislabs.plaguesky.PlagueSky;
 import com.tardislabs.plaguesky.blocks.BlockRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraft.server.level.ServerPlayer;
 
 @Mod.EventBusSubscriber(modid = PlagueSky.MODID)
 public class RespawnEvent {
-
     @SubscribeEvent
-    public static void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        Player player = event.getEntity();
-        ServerPlayer serverPlayer = (ServerPlayer) event.getEntity();
+    public static void onEntityJoin(EntityJoinLevelEvent event) {
+        Entity entity = event.getEntity();
+        if (!(entity instanceof ServerPlayer)) return;
+
+        Player player = (Player) entity;
+        ServerPlayer serverPlayer = (ServerPlayer) entity;
         Level level = player.level();
         
         // Skip if the event already has a respawn position (bed/anchor/etc)
@@ -29,12 +32,17 @@ public class RespawnEvent {
 
         BlockPos pos = player.blockPosition();
         BlockState block = level.getBlockState(pos);
-
+        if (level.dimension().location().getPath() != "overworld") return;
+        
         // Pick a safe fallback pos (adjust to your liking)
         BlockPos safe = level.getSharedSpawnPos(); // World spawn
 
         // start at plague height and go down until we find a non air or non dragon skin block
         int y = Config.COMMON.plagueHeight.get();
+        // SKip if player is below plague height
+        if (pos.getY() < y ) return;
+        PlagueSky.mutter("Saving player from plague");
+        
         while (y > 0) {
             y--;
             if (
